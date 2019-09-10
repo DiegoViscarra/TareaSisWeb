82daa598cfba5176c9c829c28e9e744a71307a58
Diego <50609792+DiegoViscarra@users.noreply.github.com>
2019-09-10 11:44:07 -0400

    Add files via upload

---
+++ TareaTecWeb-Siles-Viscarra/ProyectoRestaurante/ProyectoRestaurante/Mesero.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProyectoRestaurante
+{
+    public static class Mesero
+    {
+        public static void LimpiaMesa()
+        {
+            var limpiar = new Limpiar();
+            limpiar.PutChickenInTheOven();
+        }
+        public static void MostrarMenu()
+        {
+            int cont = 1;
+            var devs = Solemio.Platos();
+            var groupname = devs.GroupBy(n => n.Nombre);
+            Console.WriteLine("\t\t\t\t\tMENU");
+            foreach (var nom in devs)
+            {
+                Console.WriteLine(cont+".-"+nom.GetPlatillos());
+                cont++;
+            }
+        }
+        public class Limpiar
+        {
+            public void PutChickenInTheOven()
+            {
+                Console.WriteLine("\tMesero esta limpiando las mesa");
+                Thread.Sleep(2000);
+                Console.WriteLine("\tLas mesas estan limpiada");
+            }
+        }
+    }
+}
